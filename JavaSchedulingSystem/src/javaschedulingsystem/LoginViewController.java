6469b688c4561079b4b4c782b67c8677647e6ee4
Matt <matt.leonard.22@gmail.com>
2020-09-08 16:13:29 -0700

    Adds Login View

---
+++ JavaSchedulingSystem/src/javaschedulingsystem/LoginViewController.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaschedulingsystem;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+
+public class LoginViewController implements Initializable {
+
+    // FXML variables for view controls
+    @FXML private Button exitButton;
+    @FXML private Button submitButton;
+    @FXML private TextField usernameField;
+    @FXML private TextField passwordField;
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // Do I need to do anything upon initialize?
+    }    
+    
+    
+    public void loginSubmitButtonHandler(ActionEvent event) throws IOException {
+        // Eventually this will actually go to the database
+        // and authorize/authenticate user
+        
+        // TODO TEMPORARY - change scene to Appointments View to bypass login functionality
+//            Parent parent = FXMLLoader.load(getClass().getResource("AppointmentsView.fxml"));
+//            Scene scene = new Scene(parent);
+//            Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+//            window.setScene(scene);
+//            window.show();
+        
+    }
+    
+    
+    public void loginExitButtonHandler(ActionEvent event) {
+        // TODO
+        // Implement loginExitButtonHandler
+    }
+    
+    
+    
+}
