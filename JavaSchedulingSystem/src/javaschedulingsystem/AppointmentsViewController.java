b6aa9584550d377c3467b487c90cfc86d0725ba0
Matt <matt.leonard.22@gmail.com>
2020-09-08 19:11:01 -0700

    Adds Appts View

---
+++ JavaSchedulingSystem/src/javaschedulingsystem/AppointmentsViewController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaschedulingsystem;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+
+
+public class AppointmentsViewController implements Initializable {
+
+    @FXML private Button addApptButton;
+    @FXML private RadioButton viewWeekRadio;
+    @FXML private RadioButton viewMonthRadio;
+ 
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
