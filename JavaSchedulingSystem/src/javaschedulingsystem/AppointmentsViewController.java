042b376bef782e0b745fb4697a09dd66e624ffe5
Matt <matt.leonard.22@gmail.com>
2020-09-10 11:19:48 -0700

    Add Appt button changes scene

--- JavaSchedulingSystem/src/javaschedulingsystem/AppointmentsViewController.java
+++ JavaSchedulingSystem/src/javaschedulingsystem/AppointmentsViewController.java
@@ -1,16 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javaschedulingsystem;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
+import javafx.stage.Stage;
 
 
 public class AppointmentsViewController implements Initializable {
@@ -23,5 +26,14 @@ public class AppointmentsViewController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    // Change scene to Add Appt View
+    public void addApptButtonHandler(ActionEvent event) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("AddAppointmentView.fxml"));
+        Scene scene = new Scene(parent);
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+    }
     
 }
