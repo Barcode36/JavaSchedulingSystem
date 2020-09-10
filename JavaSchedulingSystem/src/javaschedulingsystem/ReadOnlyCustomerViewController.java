b5ae59342361468dd96b74952f06c7b7347678f5
Matt <matt.leonard.22@gmail.com>
2020-09-10 09:27:56 -0700

    Adds read only customer view

---
+++ JavaSchedulingSystem/src/javaschedulingsystem/ReadOnlyCustomerViewController.java
@@ -0,0 +1,31 @@
+
+package javaschedulingsystem;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+
+public class ReadOnlyCustomerViewController implements Initializable {
+
+    // FXML variables for view controls
+    @FXML private Label firstNameLabel;
+    @FXML private Label lastNameLabel;
+    @FXML private Label addressLabel;
+    @FXML private Label phoneLabel;
+    @FXML private Button closeButton;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    // TODO: This class only needs to get customer data from dB
+    // and display in the view
+    
+    // TODO: Close button handler
+    
+}
