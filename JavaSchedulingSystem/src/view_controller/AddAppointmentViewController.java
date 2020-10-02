60c4c9d66072ba2bcf751d282447418a77c8c7ed
Matt <matt.leonard.22@gmail.com>
2020-10-01 21:33:10 -0700

    Prepping add appt uui

--- JavaSchedulingSystem/src/view_controller/AddAppointmentViewController.java
+++ JavaSchedulingSystem/src/view_controller/AddAppointmentViewController.java
@@ -1,15 +1,24 @@
 
 package view_controller;
 
+import dao.CustomerDao;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import models.CustomerShort;
 import utilities.Utils;
 
 
@@ -17,15 +26,41 @@ public class AddAppointmentViewController implements Initializable {
 
     // FXML variables for view controls
     @FXML private ChoiceBox apptTypeChoice;
-    @FXML private TextField dateField;
-    @FXML private TextField timeField;
+    @FXML private DatePicker dateField;
+    @FXML private ComboBox timeField;
     @FXML private ChoiceBox customerChoice;
     @FXML private Button cancelButton;
     @FXML private Button saveButton;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        // Add types of appointments to appointment type choicebox
+        ObservableList<String> availableChoices = FXCollections.observableArrayList("Concert", "Gig", 
+                                                                                    "Practice", "Festival",
+                                                                                    "Tour stop"); 
+        apptTypeChoice.setItems(availableChoices);
+        apptTypeChoice.setValue("Concert");
+        
+        // Fetch all customers, put their names in an array and add them to customer choicebox
+        ObservableList<String> customerNames = FXCollections.observableArrayList();
+        try {
+            ObservableList<CustomerShort> customers = CustomerDao.getAllCustomers();
+            
+            for(CustomerShort cs : customers) {
+                customerNames.add(cs.getCustomerName());
+            }
+            customerChoice.setItems(customerNames);
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(AddAppointmentViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        // Set options for appointment time slots
+        ObservableList<String> times = FXCollections.observableArrayList("12a, 1230a, 1a, 130a, 2a, 230a");
+        timeField.setItems(times);
+        
+        
     }    
     
     public void saveButtonHandler(ActionEvent event) throws IOException {
