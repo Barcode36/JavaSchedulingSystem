0cd793e75dd4337a3ce6273cbc4bd81f831e8dbe
Matt <matt.leonard.22@gmail.com>
2020-10-10 10:44:50 -0700

    Adds consultant schedule report

--- JavaSchedulingSystem/src/view_controller/ConsultantScheduleReportController.java
+++ JavaSchedulingSystem/src/view_controller/ConsultantScheduleReportController.java
@@ -1,27 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package view_controller;
 
+import dao.AppointmentDao;
+import dao.UserDao;
+import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+import models.ConsultantSchedule;
+import models.User;
 
-/**
- * FXML Controller class
- *
- * @author Matty
- */
+
 public class ConsultantScheduleReportController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    // FXML variables for view controls
+    @FXML private Button backButton;
+    @FXML private TextArea textBlock;
+    User user;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        try {
+            ObservableList<ConsultantSchedule> consultantSchedules = UserDao.getAllConsultantSchedules();
+            for(ConsultantSchedule cs : consultantSchedules) {
+                textBlock.appendText("--------------------------------------------\n");
+                textBlock.appendText("User: " + cs.getUserName() + "\n");
+                textBlock.appendText("Appointment Type: " + cs.getType() + "\n");
+                textBlock.appendText("Customer Name: " + cs.getCustomerName() + "\n");
+                textBlock.appendText("Appointment Start Time: " + cs.getStart() + "\n");
+                textBlock.appendText("--------------------------------------------\n\n\n");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ConsultantScheduleReportController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    // Set current user of application
+    public void initUser(User user) {
+        this.user = user;
     }    
+    
+    
+    
+     public void backButtonHandler(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass()
+                                           .getResource("AppointmentsView.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        
+        AppointmentsViewController controller = loader.getController();
+        controller.initUser(user);
+        stage.show();
+    }
+    
     
 }
